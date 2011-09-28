11f8f11e5ebfcfb29fa53cca322ecbcbfcad9d2e
descent <descent0@gmail.com>
2011-09-28 15:19:07 +0800

    modified:   5M_mem_rw_mix_c.S     add .globl DispAL export DispAL for C code.
    modified:   k.c
        use DispAL for test the c code.

--- src/chapter3/1.5/k.c
+++ src/chapter3/1.5/k.c
@@ -3,15 +3,17 @@ void kmain( void* mbd, unsigned int magic )
 //void kmain()
 {
 #if 1
+   void DispAL(void);
    mbd=0;
    magic=0;
+#if 0
    if ( magic != 0x2BADB002 )
    {
       /* Something went not according to specs. Print an error */
       /* message and halt, but do *not* rely on the multiboot */
       /* data structure. */
    }
- 
+#endif 
    /* You could either use multiboot.h */
    /* (http://www.gnu.org/software/grub/manual/multiboot/multiboot.html#multiboot_002eh) */
    /* or do your offsets yourself. The following is merely an example. */ 
@@ -19,7 +21,9 @@ void kmain( void* mbd, unsigned int magic )
  
    /* Print a letter to screen to see everything is working: */
    unsigned char *videoram = (unsigned char *) 0xb8000;
-   videoram[0] = 65; /* character 'A' */
-   videoram[1] = 0x07; /* light grey (7) on black (0). */
+   videoram[160] = 65; /* character 'A' */
+   videoram[161] = 0x07; /* light grey (7) on black (0). */
+   asm("movb $0xab, %al");
+   DispAL();
 #endif
 }
