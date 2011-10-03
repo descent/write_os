becd56a8e58290c3f50585995f1b8200b9d79deb
descent <descent0@gmail.com>
2011-10-03 17:22:50 +0800

    modified:   5M_mem_rw_mix_c.S     test the switch_vga_mode code. modified:   switch_vga_mode.c     add switch vga mode code. modified:   makefile     add switch_vga_mode.c rule.

--- src/chapter3/1.5/switch_vga_mode.c
+++ src/chapter3/1.5/switch_vga_mode.c
@@ -11,6 +11,50 @@ typedef unsigned short int u16;
 typedef unsigned short word;
 typedef unsigned long dword;
 
+u8 io_in8(u16 port)
+{
+#if 0
+  u8 data;
+
+  // in $0x92, %al
+  asm("inb $0x92, %0"
+      : "=a"(data)
+      : );
+      #endif
+
+
+// ref: http://wiki.osdev.org/Inline_Assembly/Examples
+  u8 ret;
+  asm volatile( "inb %1, %0"
+               : "=a"(ret) : "Nd"(port) );
+  return ret;
+}
+
+#if 0
+// ref: http://wiki.osdev.org/Inline_Assembly/Examples
+static inline
+void outb( unsigned short port, unsigned char val )
+{
+    asm volatile( "outb %0, %1"
+                      : : "a"(val), "Nd"(port) );
+		      }
+#endif
+
+void io_out8(u16 port, u8 data)
+{
+//    outb    %al, $0x92
+// N: "N" : Constant in range 0 to 255 (for out instruction).
+// ref: http://www.ibiblio.org/gferg/ldp/GCC-Inline-Assembly-HOWTO.html#s6
+  asm volatile("outb %0, %1"
+               : : "a"(data), "Nd"(port) );
+}
+
+void io_out16(u16 port, u16 data)
+{
+  asm volatile("outw %0, %1"
+               : : "a"(data), "Nd"(port) );
+}
+
 #define SZ(x) (sizeof(x)/sizeof(x[0]))
 
 
@@ -77,8 +121,8 @@ static const byte height_564[] = { 0x62, 0xf0, 0x60, 0x37, 0x89,
 static const byte height_600[] = { 0x70, 0xf0, 0x60, 0x5b, 0x8c,
 0x57, 0x58, 0x70 };
 
-// the chain4 parameter should be 1 for normal 13h-type mode, but //
-only allows 320x200 256x200, 256x240 and 256x256 because you
+// the chain4 parameter should be 1 for normal 13h-type mode, but 
+// only allows 320x200 256x200, 256x240 and 256x256 because you
 // can only access the first 64kb
 
 // if chain4 is 0, then plane mode is used (tweaked modes), and
@@ -91,6 +135,10 @@ int init_graph_vga(int width, int height,int chain4)
    const byte *w,*h;
    byte val;
    int a;
+
+   width=320;
+   height=200;
+   chain4=1;
 
    switch(width) {
       case 256: w=width_256; val=R_COM+R_W256; break;
@@ -121,49 +169,99 @@ int init_graph_vga(int width, int height,int chain4)
 
    // here goes the actual modeswitch
 
-   outp(0x3c2,val);
-   outpw(0x3d4,0x0e11); // enable regs 0-7
+   io_out8(0x3c2,val);
+   io_out16(0x3d4,0x0e11); // enable regs 0-7
 
    for(a=0;a<SZ(hor_regs);++a) 
-      outpw(0x3d4,(word)((w[a]<<8)+hor_regs[a]));
+      io_out16(0x3d4,(word)((w[a]<<8)+hor_regs[a]));
    for(a=0;a<SZ(ver_regs);++a)
-      outpw(0x3d4,(word)((h[a]<<8)+ver_regs[a]));
+      io_out16(0x3d4,(word)((h[a]<<8)+ver_regs[a]));
 
-   outpw(0x3d4,0x0008); // vert.panning = 0
+   io_out16(0x3d4,0x0008); // vert.panning = 0
 
    if(chain4) {
-      outpw(0x3d4,0x4014);
-      outpw(0x3d4,0xa317);
-      outpw(0x3c4,0x0e04);
+      io_out16(0x3d4,0x4014);
+      io_out16(0x3d4,0xa317);
+      io_out16(0x3c4,0x0e04);
    } else {
-      outpw(0x3d4,0x0014);
-      outpw(0x3d4,0xe317);
-      outpw(0x3c4,0x0604);
+      io_out16(0x3d4,0x0014);
+      io_out16(0x3d4,0xe317);
+      io_out16(0x3c4,0x0604);
    }
 
-   outpw(0x3c4,0x0101);
-   outpw(0x3c4,0x0f02); // enable writing to all planes
-   outpw(0x3ce,0x4005); // 256color mode
-   outpw(0x3ce,0x0506); // graph mode & A000-AFFF
+   io_out16(0x3c4,0x0101);
+   io_out16(0x3c4,0x0f02); // enable writing to all planes
+   io_out16(0x3ce,0x4005); // 256color mode
+   io_out16(0x3ce,0x0506); // graph mode & A000-AFFF
 
-   inp(0x3da);
-   outp(0x3c0,0x30); outp(0x3c0,0x41);
-   outp(0x3c0,0x33); outp(0x3c0,0x00);
+   io_in8(0x3da);
+   io_out8(0x3c0,0x30); io_out8(0x3c0,0x41);
+   io_out8(0x3c0,0x33); io_out8(0x3c0,0x00);
 
    for(a=0;a<16;a++) {    // ega pal
-      outp(0x3c0,(byte)a); 
-      outp(0x3c0,(byte)a); 
+      io_out8(0x3c0,(byte)a); 
+      io_out8(0x3c0,(byte)a); 
    } 
    
-   outp(0x3c0, 0x20); // enable video
+   io_out8(0x3c0, 0x20); // enable video
 
    return 1;
 }
 
-void io_out8(u16 port, u8 data)
+
+// ref: 30 天打造 OS, projects/04_day sample code
+void init_palette(void)
 {
+  void set_palette(int start, int end, unsigned char *rgb);
+
+	static unsigned char table_rgb[16 * 3] = {
+		0x00, 0x00, 0x00,	/*  0: */
+		0xff, 0x00, 0x00,	/*  1:¾é¢Ô */
+		0x00, 0xff, 0x00,	/*  2:¾é¢Î */
+		0xff, 0xff, 0x00,	/*  3:¾é¢©F */
+		0x00, 0x00, 0xff,	/*  4:¾é¢Â */
+		0xff, 0x00, 0xff,	/*  5:¾é¢ */
+		0x00, 0xff, 0xff,	/*  6:¾é¢F */
+		0xff, 0xff, 0xff,	/*  7: */
+		0xc6, 0xc6, 0xc6,	/*  8:¾é¢DF */
+		0x84, 0x00, 0x00,	/*  9:Ã¢Ô */
+		0x00, 0x84, 0x00,	/* 10:Ã¢Î */
+		0x84, 0x84, 0x00,	/* 11:Ã¢©F */
+		0x00, 0x00, 0x84,	/* 12:Ã¢Â */
+		0x84, 0x00, 0x84,	/* 13:Ã¢ */
+		0x00, 0x84, 0x84,	/* 14:Ã¢F */
+		0x84, 0x84, 0x84	/* 15:Ã¢DF */
+	};
+	set_palette(0, 15, table_rgb);
+	return;
+
+	/* static char ½ßÍAf[^Éµ©g¦È¢¯ÇDB½ß */
 }
 
-void io_out16(u16 port, u16 data)
+void set_palette(int start, int end, unsigned char *rgb)
 {
+	int i, eflags;
+	//eflags = io_load_eflags();	/* èÝÂtOÌlðL^·é */
+	//io_cli(); 					/* ÂtOð0ÉµÄèÝÖ~É·é */
+	io_out8(0x03c8, start);
+	for (i = start; i <= end; i++) {
+		io_out8(0x03c9, rgb[0] / 4);
+		io_out8(0x03c9, rgb[1] / 4);
+		io_out8(0x03c9, rgb[2] / 4);
+		rgb += 3;
+	}
+	//io_store_eflags(eflags);	/* èÝÂtOð³Éß· */
+	return;
 }
+
+
+/*
+ *  Hope you get what you want. To put on normal mode 13h, use
+ *  init_graph_vga(320,200,1);
+*/
+#if 0
+int main(int argc, char **argv)
+{
+  init_graph_vga(320,200,1);
+}
+#endif
