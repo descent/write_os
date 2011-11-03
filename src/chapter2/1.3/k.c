cd9f108303d271b23cc4a4527e275bcbcf872b77
descent <descent@ubuntu.ubuntu-domain>
2011-11-03 11:34:38 +0800

    modified:   k.c     add hex2char for test c runtime.

--- src/chapter2/1.3/k.c
+++ src/chapter2/1.3/k.c
@@ -9,10 +9,36 @@ void write_mem8(u32 addr, u8 data); // assembly function.
 void DispAL();
 void DispAX();
 
+void hex2char(u8 h, u8 *ch)
+{
+  // high 4 bit
+  //ch[1] 
+  if (((h >> 4)& 0x0f) >= 0xa) // a-f
+  {
+    ch[1] = ((h >> 4) & 0x0f) + 0x57;
+  }
+  else // 0-9
+  {
+    ch[1] = ((h >> 4) & 0x0f) + 0x30;
+  }
+
+  // low 4 bit
+  if ((h & 0x0f) >= 0xa) // a-f
+  {
+    ch[0] = (h & 0x0f) + 0x57;
+  }
+  else // 0-9
+  {
+    ch[0] = (h & 0x0f) + 0x30;
+  }
+
+}
+
+
 //void kmain( void* mbd, unsigned int magic )
 void kmain(void)
 {
-#if 1
+#if 0
   int i=0;
   u8 ch='A';
 
@@ -23,7 +49,14 @@ void kmain(void)
       ch='A';
   }
 #else
-  write_mem8(160*2, 'N');
+  //write_mem8(160*2, 'N');
+  u8 ch[2]={'a','b'}; // 97, 98
+
+  hex2char(0x9a, ch);
+  write_mem8(160*2, ch[1]); // high 4 bits
+  write_mem8(160*2+2, ch[0]); // low 4 bits
+
+
 #endif 
 //  u8 b[10];
   
